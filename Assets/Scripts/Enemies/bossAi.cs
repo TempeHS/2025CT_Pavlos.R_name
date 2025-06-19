66c4c8be6d12aa56e45e2ec333910be671da0f1e
PavlosR <pavlos.rorris@education.nsw.gov.au>
2025-06-19 15:04:18 +1000

    radial attack

--- Assets/Scripts/Enemies/bossAi.cs
+++ Assets/Scripts/Enemies/bossAi.cs
@@ -12,11 +12,14 @@ public class bossAi : MonoBehaviour
     [SerializeField] public float knockback;
     [SerializeField] public float health;
     private GameObject Proj1;
+    [SerializeField] private GameObject Proj2;
 
     private float attacktime;
 
     private bool attacking = false;
     private bool lineOfSight = false;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +29,7 @@ public class bossAi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        attack1();
     }
 
     void FixedUpdate()
@@ -44,17 +47,29 @@ public class bossAi : MonoBehaviour
 
     void attack1()
     {
-        Instantiate(Proj1, gameObject.transform);
+        radProj(10);
     }
 
     void radProj(int numProj)
     {
-
+        Vector2 startPoint = transform.position;
+        int radius = 1;
         float angle = 0f;
+        float angleStep = 360f / numProj;
+        float moveSpeed = 2;
 
         for (int i = 0; i <= numProj - 1; i++)
         {
             float projectileDirXposition = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+            float projectileDirYposition = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+
+            Vector2 projectileVector = new Vector2(projectileDirXposition, projectileDirYposition);
+            Vector2 projectileMoveDirection = (projectileVector - startPoint).normalized * moveSpeed;
+
+            var proj = Instantiate(Proj2, startPoint, Quaternion.identity);
+            proj.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
+
+            angle += angleStep;
         }
     }
 }
